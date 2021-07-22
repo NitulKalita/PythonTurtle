d6252e8737b0487fa03a6030bae5d5ae80410492
Nitul <nitulkalita@protonmail.com>
2021-07-22 09:57:41 -0700

    file added

---
+++ octagonaldesign.py
@@ -0,0 +1,13 @@
+from turtle import *
+import turtle
+speed(10)
+bgcolor("black")
+color("yellow")
+pensize(5)
+for i in range(8):
+    left(45)
+    for i in range(8):
+        forward(100)
+        right(45)
+
+turtle.done()
