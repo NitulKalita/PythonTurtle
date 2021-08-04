3dfdb71563c8a07a4bf8d8393a81a597a60103cf
Nitul <nitulkalita@protonmail.com>
2021-08-04 06:28:49 -0700

    file added

---
+++ Olympic Rings.py
@@ -0,0 +1,35 @@
+import turtle as t
+
+t.pensize(5)
+
+t.color("blue")
+t.penup()
+t.goto(-110, -25)
+t.pendown()
+t.circle(45)
+
+t.color("black")
+t.penup()
+t.goto(0, -25)
+t.pendown()
+t.circle(45)
+
+t.color("red")
+t.penup()
+t.goto(110, -25)
+t.pendown()
+t.circle(45)
+
+t.color("yellow")
+t.penup()
+t.goto(-55, -75)
+t.pendown()
+t.circle(45)
+
+t.color("green")
+t.penup()
+t.goto(55, -75)
+t.pendown()
+t.circle(45)
+
+t.done()
